2a4e63895bfb5e42198c4edf6d159cda4f139cbd
RedEmber-hub <redember38@gmail.com>
2026-03-01 18:47:13 +0300

    remove link card

--- js/script.js
+++ js/script.js
@@ -87,7 +87,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function createCardHTML(value) {
     return `
-    <a href="/card" class="card-link">
       <article class="card" data-id="${value.id}">
         <div class="card__header">
 
@@ -149,7 +148,6 @@ document.addEventListener('DOMContentLoaded', function () {
             <div class="card__button-link">
               <div class="card__actions">
                 <button class="like-button ${value.isLiked ? 'is-liked' : ''}">
-                  <!-- INLINE SVG для лайка -->
                   <svg class="icon icon-like" width="24" height="24" viewBox="0 0 24 24">
                     <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
                   </svg>
@@ -165,8 +163,7 @@ document.addEventListener('DOMContentLoaded', function () {
           </div>
         </div>
       </article>
-    </a>
-  `;
+    `;
   }
 
   function updateCardDOM(cardElement, cardData) {
